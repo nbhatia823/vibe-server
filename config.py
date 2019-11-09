8e4d1e7b0ff46a40dbda7148d17611362c0164da
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-11-08 23:14:17 -0800

    basics to trigger heroku build

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
 
 class Config:
-    pass
+	HOST = 'ec2-174-129-253-140.compute-1.amazonaws.com'
+	DATABASE = 'd9ku25a2bulpk2'
+	USERNAME = 'bvxxebjsjrhphb'
+	PASSWORD = '596f67861a11f2d606078c579ee8aa2f8c8855297aff08e3d0a3f60d1b0f8eed'
+	PORT = '5432'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
