546a2d378d7ec786e80594fee70bcc3c36e1a08a
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-11-11 16:02:25 -0800

    User CRUD api

--- api.py
+++ api.py
@@ -1,10 +1,59 @@
 from flask import request, json, Response, Blueprint
-from classes.user import User
+from classes.user import User, create_user, get_user, update_user, delete_user
 
-api_routes = Blueprint('pra_api', __name__)
+
+api_routes = Blueprint('api_routes', __name__)
 
 ### USER API ###
 @api_routes.route('/api/user/<user_id>', methods=['GET','POST','PUT'])
 def handle_user_request(user_id):
     if request.method == 'GET':
         return User.get_user(user_id)
+
+@api_routes.route('/api/user', methods=['POST'])
+def post_user():
+    if request.method == 'POST':
+        print("attempting to post new request")
+        user_field_mappings = get_user_info_from_current_request()
+        new_user_id = create_user(user_field_mappings)
+        if new_user_id != -1:
+            return Response(status=201)
+        else:
+            return Response(status=400)
+
+@api_routes.route('/api/user/<user_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_or_update_or_delete_user(user_id): # 'id' is string-type ?
+    
+    if request.method == 'PUT':
+        user_field_mappings = get_user_info_from_current_request()
+        rows_updated = update_user(id, user_field_mappings)
+        if rows_updated == 1:
+            return Response(status=204)
+        else:
+            return Response(status=400)
+    
+    elif request.method == 'GET':
+        user_dict = get_user(id)
+        # if the user with given id exists, send it back as json;
+        if user_dict:
+            user_json = json.dumps(user_dict)
+            return Response(user_json,  
+                        mimetype='application/json', 
+                        status=200)
+        # else if not found, return 404 code saying not found
+        else:
+            return Response(status=404)
+    
+    elif request.method == 'DELETE':
+        rows_deleted = delete_user(id)
+        if rows_deleted == 1:
+            return Response(status=204)
+        else:
+            return Response(status=400)
+            
+# reads field_mappings from body of request and returns as a dictionary of field_name: field_value; 
+# must be a json-type header in request
+def get_user_info_from_current_request():
+    field_mappings = request.json
+    return field_mappings
+
--- classes/user.py
+++ classes/user.py
@@ -2,14 +2,50 @@ from peewee import CharField, DecimalField
 from peewee_setup import BaseModel, db
 
 class User(BaseModel):
-    user_id = CharField()
+    user_id = CharField(primary_key=True)
     user_name = CharField()
     profile_pic_url = CharField()
     sentiment_score = DecimalField()
     auth_token = CharField(null=True)
 
-def get_user(user_id):
-    return User.get_by_id(user_id)
-
 db.create_tables([User])
+
+
+# Get a user as dict by id
+# Args: user_id (string/int of user_id)
+# Return: User as dictionary if found, None if not found
+def get_user(id):
+    try:
+        user_array = User.select().where(User.id == user_id).limit(1).dicts().execute()
+        return user_array[0]
+    # in case of user not found or incorrect parameter user_id
+    except:
+        return None
+
+# Create a user with provided fields
+# Args: User_fields (dictionary- key = field, value = field_value)
+# Return: Number of rows created, -1 if error
+def create_user(user_fields):
+    try:
+        return User.insert(**user_fields).execute()
+    # in case of incorrect user field mapping
+    except:
+        return -1 
+
+
+# Updates user using field_mapping
+# Args: user_id (string/int of user_id), User_fields (dictionary- key = field, value = field_value)
+# Return: Number of rows updated, -1 if error
+def update_user(user_id, user_fields):
+    try:
+        return User.update(**field_mapping).where(User.user_id == user_id).execute()
+    # in case of incorrect user field mapping
+    except:
+        return -1
+
+# Deletes a user using id
+# Args: user_id (string/int of user_id)
+# Return: Number of rows deleted
+def delete_user(user_id):
+    return User.delete().where(User.user_id == user_id).execute()
 
