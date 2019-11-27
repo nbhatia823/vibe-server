1f9d77e7b00a9d93fd1f1d2be3926df5a24eef69
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-11-27 15:38:46 -0800

    [API] - Change POST user endpoint to PUT allowing for Upserts, and formatting

--- api.py
+++ api.py
@@ -1,59 +1,56 @@
 from flask import request, json, Response, Blueprint
-from classes.users import Users, create_user, get_user, update_user, delete_user
+from classes.users import Users, get_user, update_user, delete_user, create_user
 from classes.track import Track, create_track, get_track, update_track, delete_track
 from classes.spotify_helper import SpotifyAPI
 
 api_routes = Blueprint('api_routes', __name__)
 
 ### USER API ###
-@api_routes.route('/api/users', methods=['POST'])
-def user_post_handler():
+@api_routes.route('/api/users', methods=['PUT'])
+def user_put_handler():
     """
-    Creates a new user
+    Creates a new user OR updates user if they already exist
     Input: HTTP POST request with JSON body of parameters
     Output: HTTP Response, status 201 if successful, 400 if failed
     """
-    if request.method == 'POST':
-        print("attempting to post new request")
+
+    if request.method == 'PUT':
         user_field_mappings = get_json_body_from_current_request()
-        new_user_id = create_user(user_field_mappings)
-        if new_user_id != -1:
-            print("new user id is ", new_user_id)
-            return Response(status=201)
+        print(user_field_mappings)
+        user_id = user_field_mappings["user_id"]
+        if get_user(user_id) == None:
+            # Create user
+            rows_updated = create_user(user_field_mappings)
+        else:
+            rows_updated = update_user(user_id, user_field_mappings)
+        print(rows_updated)
+        if rows_updated == 1:
+            return Response(status=204)
         else:
             return Response(status=400)
 
-@api_routes.route('/api/users/<user_id>', methods=['GET', 'PUT', 'DELETE'])
-def user_get_or_update_or_delete_handler(user_id): # 'user_id' is string-type
+
+@api_routes.route('/api/users/<user_id>', methods=['GET', 'DELETE'])
+def user_get_or_delete_handler(user_id):  # 'user_id' is string-type
     """
     Gets, updates, or deletes an existing user
     Input: HTTP GET. Output: JSON, status 200, or status 404
     Input: HTTP POST. Output: 204 or 400.
     Input: HTTP DELETE. Output: 204, or 400.
     """
-    print("received user request for id ", user_id)
-    print("are types equal", (102123121111 == user_id), (user_id == "102123121111" ))
+
     if request.method == 'GET':
         user_dict = get_user(user_id)
         # if the user with given id exists, send it back as json
         if user_dict:
             user_json = json.dumps(user_dict)
-            return Response(user_json,  
-                        mimetype='application/json', 
-                        status=200)
+            return Response(user_json,
+                            mimetype='application/json',
+                            status=200)
         # else if not found, return 404 code saying not found
         else:
             return Response(status=404)
 
-    elif request.method == 'PUT':
-        user_field_mappings = get_json_body_from_current_request()
-        rows_updated = update_user(user_id, user_field_mappings)
-        print(rows_updated)
-        if rows_updated == 1:
-            return Response(status=204)
-        else:
-            return Response(status=400)
-    
     elif request.method == 'DELETE':
         rows_deleted = delete_user(user_id)
         if rows_deleted == 1:
@@ -78,28 +75,30 @@ def post_track_handler():
         else:
             return Response(status=400)
 
+
 @api_routes.route('/api/track/<track_id>', methods=['PUT', 'GET', 'DELETE'])
-def track_get_or_update_or_delete_handler(track_id): # 'id' is string-type ?
+def track_get_or_update_or_delete_handler(track_id):  # 'id' is string-type ?
     """
+
     Get, update, or delete 
     Input: PUT. Output: status 204 or 400.
     Input: GET. Output: JSON + status 200. or status 404
     Input: DELETE. Output: Status 204 or 400.
     """
     print("received track request for id ", track_id)
-    
+
     if request.method == 'GET':
         track_dict = get_track(track_id)
         # if the track with given id exists, send it back as json;
         if track_dict:
             track_json = json.dumps(track_dict)
-            return Response(track_json,  
-                        mimetype='application/json', 
-                        status=200)
+            return Response(track_json,
+                            mimetype='application/json',
+                            status=200)
         # else if not found, return 404 code saying not found
         else:
             return Response(status=404)
-    
+
     elif request.method == 'PUT':
         track_field_mappings = get_json_body_from_current_request()
         rows_updated = update_track(track_id, track_field_mappings)
@@ -107,7 +106,7 @@ def track_get_or_update_or_delete_handler(track_id): # 'id' is string-type ?
             return Response(status=204)
         else:
             return Response(status=400)
-    
+
     elif request.method == 'DELETE':
         rows_deleted = delete_track(track_id)
         if rows_deleted == 1:
@@ -130,6 +129,7 @@ def post_track_of_day_handler(user_id, track_id):
         else:
             return Response(status=404)
 
+
 @api_routes.route('/api/users/<user_id>/post', methods=['DELETE'])
 def delete_track_of_day_handler(user_id):
     """Remove the user's track of the day.
@@ -138,6 +138,7 @@ def delete_track_of_day_handler(user_id):
     """
     if request.method == 'DELETE':
         pass
+
 
 @api_routes.route('/api/users/<user_id>/post/history', methods=['GET'])
 def get_track_of_day_history_handler(user_id):
@@ -156,6 +157,7 @@ def get_user_feed_handler(user_id):
     """
     if request.method == 'GET':
         pass
+
 
 @api_routes.route('/api/users/<user_id>/history', methods=['GET'])
 def get_recently_played(user_id):
@@ -176,14 +178,14 @@ def get_friend_list_handler(user_id):
     if request.method == 'POST':
         pass
 
+
 @api_routes.route('/api/users/<user_id>/friends/<friend_id>', methods=['POST', 'DELETE'])
 def add_or_delete_friend_handler(user_id, track_id):
     if request.method == 'POST':
         pass
 
 
-
-# reads field_mappings from body of request and returns as a dictionary of field_name: field_value; 
+# reads field_mappings from body of request and returns as a dictionary of field_name: field_value;
 # must be a json-type header in request
 def get_json_body_from_current_request():
     field_mappings = request.json
@@ -192,10 +194,10 @@ def get_json_body_from_current_request():
 
 ### Spotify Redirect URI ###
 @api_routes.route('/api/spotify_callback', methods=['GET'])
-#TODO: how to make these function definitions?
+# TODO: how to make these function definitions?
 def handler():
     # we get a code to exchange for access token
-    #TODO: parse the response query string for error and code?
+    # TODO: parse the response query string for error and code?
     code = 1
     helper = SpotifyAPI()
     helper.request_user_tokens(code)
