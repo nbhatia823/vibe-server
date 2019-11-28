20bae335528426d40c10f77487ef702334f6c55d
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-11-28 00:30:57 -0800

    [SPOTIFY] - Setup auth token from Spotify

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, Blueprint
 from api import api_routes
+from config import Config
+import threading
 
 app = Flask(__name__)
 app.register_blueprint(api_routes)
 app.config['WTF_CSRF_ENABLED'] = False
+
+# SET AUTH TOKEN AND REFRESH EVERY 3600 seconds
+Config.setAuthToken()
+threading.Timer(3600, Config.setAuthToken).start()
 
 
 @app.route("/")
--- config.py
+++ config.py
@@ -1,9 +1,23 @@
 import os
+import requests
+
 
 class Config:
-	HOST = 'ec2-174-129-253-140.compute-1.amazonaws.com'
-	DATABASE = 'd9ku25a2bulpk2'
-	USERNAME = 'bvxxebjsjrhphb'
-	PASSWORD = '596f67861a11f2d606078c579ee8aa2f8c8855297aff08e3d0a3f60d1b0f8eed'
-	PORT = '5432'
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    HOST = 'ec2-174-129-253-140.compute-1.amazonaws.com'
+    DATABASE = 'd9ku25a2bulpk2'
+    USERNAME = 'bvxxebjsjrhphb'
+    PASSWORD = '596f67861a11f2d606078c579ee8aa2f8c8855297aff08e3d0a3f60d1b0f8eed'
+    PORT = '5432'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SPOTIFY_CLIENT_ID = '5ae1d35a487842fcbef9cb7f164ec3fb'
+    SPOTIFY_CLIENT_SECRET = 'c27f32c0e5f04c2e989acc4c05097ba5'
+    SPOTIFY_AUTH_TOKEN = ''
+
+    @staticmethod
+    def setAuthToken():
+        url = "https://accounts.spotify.com/api/token"
+        body_params = {'grant_type': 'client_credentials'}
+        authorization = (Config.SPOTIFY_CLIENT_ID,
+                         Config.SPOTIFY_CLIENT_SECRET)
+        resp = requests.post(url, data=body_params, auth=authorization)
+        Config.SPOTIFY_AUTH_TOKEN = resp.json()["access_token"]
