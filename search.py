a4ef281fbdadfdd3039e0f5f5bffee3dcce90c0e
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-11-28 15:38:13 -0800

    [SEARCH] - Change response format of search JSON to include one less nested object

--- search.py
+++ search.py
@@ -10,7 +10,7 @@ class Search:
         query.replace(" ", "%20")
         url = f'{Config.SPOTIFY_API_URL}/v1/search?q={query}&type=track&limit=4'
         resp = requests.get(url, headers=Config.SPOTIFY_REQ_HEADERS)
-        return resp.json()["tracks"]
+        return resp.json()["tracks"]["items"]
 
     @staticmethod
     def search(query):
