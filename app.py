87ee2fbf26f03b55a0bd117ca28e7d67b47e7f72
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-12-01 23:31:58 -0800

    [CORS] - Enable all CORS request

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from config import Config
 from classes.search import Search
 import threading
 from classes.spotify_helper import SpotifyHelper
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(api_routes)
 app.config['WTF_CSRF_ENABLED'] = False
 
